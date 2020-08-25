cd5457ec9674ae5ad0ad7ecd542ce241b5404dfe
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-25 18:42:31 +0200

    Initial commit

---
+++ app/javascript/components/NewRecipe.jsx
@@ -0,0 +1,116 @@
+class NewRecipe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      ingredients: "",
+      instruction: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.stripHtmlEntities = this.stripHtmlEntities.bind(this);
+  }
+
+  stripHtmlEntities(str) {
+    return String(str)
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const url = "/api/v1/recipes/create";
+    const { name, ingredients, instruction } = this.state;
+
+    if (name.length == 0 || ingredients.length == 0 || instruction.length == 0)
+      return;
+
+    const body = {
+      name,
+      ingredients,
+      instruction: instruction.replace(/\n/g, "<br> <br>")
+    };
+
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.props.history.push(`/recipe/${response.id}`))
+      .catch(error => console.log(error.message));
+  }
+
+  render() {
+    return (
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-sm-12 col-lg-6 offset-lg-3">
+            <h1 className="font-weight-normal mb-5">
+              Add a new recipe to our awesome recipe collection.
+            </h1>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="recipeName">Recipe name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="recipeName"
+                  className="form-control"
+                  required
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="recipeIngredients">Ingredients</label>
+                <input
+                  type="text"
+                  name="ingredients"
+                  id="recipeIngredients"
+                  className="form-control"
+                  required
+                  onChange={this.onChange}
+                />
+                <small id="ingredientsHelp" className="form-text text-muted">
+                  Separate each ingredient with a comma.
+                </small>
+              </div>
+              <label htmlFor="instruction">Preparation Instructions</label>
+              <textarea
+                className="form-control"
+                id="instruction"
+                name="instruction"
+                rows="5"
+                required
+                onChange={this.onChange}
+              />
+              <button type="submit" className="btn custom-button mt-3">
+                Create Recipe
+              </button>
+              <Link to="/recipes" className="btn btn-link mt-3">
+                Back to recipes
+              </Link>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default NewRecipe;
---
+++ app/javascript/routes/Index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../components/Home";
+import Recipes from "../components/Recipes";
+import Recipe from "../components/Recipe";
+import NewRecipe from "../components/NewRecipe";
+
+export default (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/recipes" exact component={Recipes} />
+      <Route path="/recipe/:id" exact component={Recipe} />
+      <Route path="/recipe" exact component={NewRecipe} />
+    </Switch>
+  </Router>
+);
